6fe88dbfb66ec55b47a6ce8c72249bacae626ba4
Conrad Harrison <92973594+Metamorphs96@users.noreply.github.com>
2021-10-24 15:34:54 +1030

    Add files via upload

---
+++ C/StructuralCalcs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float Cpe,qz,pn,w,L,M;
+
+	Cpe=-0.7;
+	qz=0.96;			//kPa
+	pn=Cpe*qz;			//kPa
+	w=pn*3.000;			//kN/m
+	L=6.000;			//m
+	M=w*pow(L,2)/8;		//kNm
+
+	printf("Moment: %5.2f" , M);
+}
---
+++ C/schReadMe.txt
@@ -0,0 +1,1 @@
+codeblocks generating files Norton considers to be a problem.Heur.AdvML.B
